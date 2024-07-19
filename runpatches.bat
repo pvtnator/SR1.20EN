c7b7c1aa2957c7ca3bf22ecc8100f95230935f25
denhonator <denhonator@gmail.com>
2024-07-19 18:07:50 +0300

    Revision up to Dark Cave

--- runpatches.bat
+++ runpatches.bat
@@ -1,3 +1,2 @@
+"..\rpgmt_cli_v4.5\rpgmt.exe" "..\Succubus Rhapsodia ver1.19"
 python ./mapConditionReplace.py
-python ./translateCharacters.py
-pause
---
+++ runtalk.bat
@@ -0,0 +1,1 @@
+python ./translateCharacters.py
