c01021af616ed8518df6a17b41a72425356ee762
denhonator <denhonator@gmail.com>
2025-03-17 02:12:40 +0200

    More skills

--- sync.py
+++ sync.py
@@ -112,12 +112,12 @@ if __name__ == "__main__":
     #source_files = [current_dir / "patch" / "Unused.txt"]
     #source_files = [current_dir / "patch" / "Armors.txt"]
     source_files = [current_dir / "patch" / "Skills.txt"]
-    source_files = [current_dir / "mapswap" / "patch" / "Skills.txt"]
+    #source_files = [current_dir / "mapswap" / "patch" / "Skills.txt"]
     #source_files += [current_dir / "patch" / "Scripts.txt"]
     #source_files += [current_dir / "patch" / "Items.txt"]
-    #dest_files = [current_dir / "mod_scripts.txt"]
+    dest_files = [current_dir / "mod_scripts.txt"]
     #dest_files += [current_dir / "talk.txt"]
-    dest_files = [current_dir / "patch" / "Skills.txt"]
+    #dest_files = [current_dir / "patch" / "Skills.txt"]
     #for file in (current_dir / "patch").rglob("*.txt"):
     #    if not "Unused" in str(file) and not "States" in str(file):
     #        dest_files.append(file)
