56412406564fa56c73820794e87206e67a3a0ca5
pvtnator <denhonator@gmail.com>
2025-04-25 00:01:58 +0300

    Some Plains/Hills

--- runpatch.bat
+++ runpatch.bat
@@ -6,4 +6,3 @@ xcopy "..\Succubus Rhapsodia_mod_translated\Mod\Mod_Data" "..\Succubus Rhapsodia
 python ./translateCharacters.py apply mod_scripts Mod_Scripts
 RD "..\Succubus Rhapsodia Mod Translated\Mod\Mod_Scripts" /S /Q
 xcopy "..\Succubus Rhapsodia_mod_translated\Mod\Mod_Scripts" "..\Succubus Rhapsodia Mod Translated\Mod\Mod_Scripts" /E /H /I /Y
-pause
