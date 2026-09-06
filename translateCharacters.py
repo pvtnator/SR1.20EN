e2009ccaabcc011e30868fa36117e78913c5406d
pvtnator <denhonator@gmail.com>
2026-09-06 13:08:18 +0300

    talk extract

--- translateCharacters.py
+++ translateCharacters.py
@@ -295,7 +295,7 @@ def apply_translations(folder_path, apply_path, regexes, translations, mustinclu
                 
 if __name__ == "__main__":
     current_dir = Path.cwd()
-    mode = sys.argv[1] if len(sys.argv)>1 else "autofix"
+    mode = sys.argv[1] if len(sys.argv)>1 else "extract"
     source = sys.argv[2] if len(sys.argv)>2 else "talk"
     dest = sys.argv[3] if len(sys.argv)>3 else "talk"
     quickpatch = ""
