cc08bf5ac45385ce365d95ceb5fcdc949c5b5d7c
pvtnator <denhonator@gmail.com>
2026-09-06 13:03:37 +0300

    old stuff

--- runpatch.bat
+++ runpatch.bat
@@ -1,4 +1,6 @@
 "..\rpgmt_cli_v4.5\rpgmt.exe" "..\Succubus Rhapsodia_mod"
+RD "..\Succubus Rhapsodia_mod_translated\Mod\Mod_Data" /S /Q
+mkdir "..\Succubus Rhapsodia_mod_translated\Mod\Mod_Data"
 python mapConditionReplace.py
 xcopy "..\Succubus Rhapsodia_mod_translated\Data" "..\Succubus Rhapsodia Mod Translated\Mod\Mod_Data" /H /C /I /Y
 xcopy "..\Succubus Rhapsodia_mod_translated\Data" "..\Succubus Rhapsodia Mod Translated\Data" /H /C /I /Y
